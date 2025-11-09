a5aecd716dee98b800a62e0a070cde2cbb844458
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-11-09 11:30:30 +0100

    add Vichy

---
+++ configurations/vichy.mjs
@@ -0,0 +1,12 @@
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	gtfsResourceHref:
+		"https://www.data.gouv.fr/api/1/datasets/r/4653683f-48a6-4f84-b313-058687fc5d04",
+	hawkId: "vichy",
+	refreshInterval: 30_000,
+	matchRoute: (gtfsRoute, hawkVehicle) => gtfsRoute.name === hawkVehicle.RouteNumber,
+	matchStopTime: (gtfsStopTime, hawkSchedule) =>
+		gtfsStopTime.stop.code === hawkSchedule.StopGraphKey,
+};
+
+export default configuration;
