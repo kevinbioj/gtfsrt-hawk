856bea30b39c877a9d34216a6b116c3deb621fdf
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-11-09 09:05:21 +0100

    add Dreux

---
+++ configurations/dreux.mjs
@@ -0,0 +1,11 @@
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	gtfsResourceHref: "https://fr.ftp.opendatasoft.com/centrevaldeloire/OKINAGTFS/GTFS_AO/DREUX.zip",
+	hawkId: "drouais",
+	refreshInterval: 30_000,
+	matchRoute: (gtfsRoute, hawkVehicle) => gtfsRoute.id === hawkVehicle.RouteNumber,
+	matchStopTime: (gtfsStopTime, hawkSchedule) =>
+		gtfsStopTime.stop.code === hawkSchedule.StopGraphKey,
+};
+
+export default configuration;
