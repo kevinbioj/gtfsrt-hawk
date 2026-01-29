2f8140500d5a1f329fb14132b6acbf7dd270438e
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-01-29 19:38:02 +0100

    niort: match NAVC/NCV properly

--- configurations/niort.mjs
+++ configurations/niort.mjs
@@ -4,7 +4,13 @@ const configuration = {
 		"https://www.pigma.org/public/opendata/nouvelle_aquitaine_mobilites/publication/ca_du_niortais-aggregated-gtfs.zip",
 	hawkId: "niort",
 	refreshInterval: 30_000,
-	matchRoute: (gtfsRoute, hawkVehicle) => gtfsRoute.name === hawkVehicle.RouteNumber,
+	matchRoute: (gtfsRoute, hawkVehicle) => {
+		if (hawkVehicle.RouteNumber === "NAVC") {
+			return gtfsRoute.name === "NCV";
+		}
+
+		return gtfsRoute.name === hawkVehicle.RouteNumber;
+	},
 	matchStopTime: (gtfsStopTime, hawkSchedule) =>
 		gtfsStopTime.stop.code === hawkSchedule?.StopGraphKey,
 };
