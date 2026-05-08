781eb1da66fd1895edc394e07b4dc9fd91b628c0
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-05-08 15:24:24 +0200

    fecamp: update configuration

--- configurations/fecamp.mjs
+++ configurations/fecamp.mjs
@@ -1,12 +1,11 @@
 /** @type {import('../src/configuration/configuration.ts').Configuration} */
 const configuration = {
-	gtfsResourceHref:
-		"https://api.atm.cityway.fr/dataflow/offre-tc/download?provider=FICIBUS&dataFormat=GTFS",
+	gtfsResourceHref: "https://api.atm.cityway.fr/dataflow/offre-tc/download?provider=FICIBUS&dataFormat=GTFS",
 	hawkId: "fecamp",
 	refreshInterval: 30_000,
-	matchRoute: (gtfsRoute, hawkVehicle) => gtfsRoute.name === hawkVehicle.RouteNumber,
-	matchStopTime: (gtfsStopTime, hawkSchedule) =>
-		gtfsStopTime.stop.code === hawkSchedule?.StopGraphKey,
+	matchRoute: (gtfsRoute, hawkVehicle) =>
+		(hawkVehicle.RouteNumber === "L10" ? "L1" : hawkVehicle.RouteNumber) === gtfsRoute.name,
+	matchStopTime: (gtfsStopTime, hawkSchedule) => gtfsStopTime.stop.code === hawkSchedule?.StopGraphKey,
 };
 
 export default configuration;
